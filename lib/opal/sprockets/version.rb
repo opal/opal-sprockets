9f0b9436147f1133654084ebab17702443f7c598
Elia Schito <elia@schito.me>
2016-09-27 23:40:02 +0200

    Add a BASE_VERSION to keep some continuity

--- lib/opal/sprockets/version.rb
+++ lib/opal/sprockets/version.rb
@@ -1,7 +1,8 @@
 module Opal
   module Sprockets
+    BASE_VERSION = '0.4'
     OPAL_VERSION = '0.11.dev'
     SPROCKETS_VERSION = '3.1'
-    VERSION = "#{OPAL_VERSION}.#{SPROCKETS_VERSION}.0"
+    VERSION = "#{BASE_VERSION}.#{OPAL_VERSION}.#{SPROCKETS_VERSION}.0"
   end
 end
