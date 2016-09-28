112d7b657ccf946385827a6ac26c10c38701c662
Elia Schito <elia@schito.me>
2016-09-28 21:56:56 +0200

    Use 3 parts for the base version number

--- lib/opal/sprockets/version.rb
+++ lib/opal/sprockets/version.rb
@@ -1,8 +1,8 @@
 module Opal
   module Sprockets
-    BASE_VERSION = '0.4'
+    BASE_VERSION = '0.4.0'
     OPAL_VERSION = '0.11.dev'
     SPROCKETS_VERSION = '3.1'
-    VERSION = "#{BASE_VERSION}.#{OPAL_VERSION}.#{SPROCKETS_VERSION}.0"
+    VERSION = "#{BASE_VERSION}.#{OPAL_VERSION}.#{SPROCKETS_VERSION}"
   end
 end
