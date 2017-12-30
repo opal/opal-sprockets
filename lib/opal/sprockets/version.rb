e056950f49c70c072c93b22bb0aa05a1e958a27c
Elia Schito <elia@schito.me>
2017-12-30 01:40:27 +0100

    Bump opal version and release

--- lib/opal/sprockets/version.rb
+++ lib/opal/sprockets/version.rb
@@ -1,7 +1,7 @@
 module Opal
   module Sprockets
     BASE_VERSION = '0.4.1'
-    OPAL_VERSION = '0.11.0.rc1'
+    OPAL_VERSION = '0.11.0'
     SPROCKETS_VERSION = '3.1'
     VERSION = "#{BASE_VERSION}.#{OPAL_VERSION}.#{SPROCKETS_VERSION}"
   end
