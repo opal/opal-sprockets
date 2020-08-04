71886354390d542231c0461bb8bc3f279dbbe78f
hmdne <54514036+hmdne@users.noreply.github.com>
2020-08-04 09:06:30 +0200

    Include a failing test - the dependencies system is faulty

--- spec/sprockets/server_spec.rb
+++ spec/sprockets/server_spec.rb
@@ -39,6 +39,29 @@ describe Opal::Sprockets::Server do
     end
   end
 
+  it 'recompiles the asset when its dependencies change' do
+    does_include = proc do |it, what, what_not=[]|
+      what.each { |i| expect(it).to include %Q'modules["required_tree_test/required_file#{i}"]' }
+      what_not.each { |i| expect(what_not).not_to include %Q'modules["required_tree_test/required_file#{i}"]' }
+    end
+
+    asset = app.sprockets['require_tree_test', accept: "application/javascript"]
+    expect(asset).to be_truthy
+    does_include.(asset.to_s, [1,2], [3])
+    get '/assets/require_tree_test.js'
+    does_include.(last_response.body, [1,2], [3])
+
+    File.write(__dir__+"/../fixtures/required_tree_test/required_file3.rb", "p 3")
+
+    asset = app.sprockets['require_tree_test', accept: "application/javascript"]
+    expect(asset).to be_truthy
+    does_include.(asset.to_s, [1,2,3])
+    get '/assets/require_tree_test.js'
+    does_include.(last_response.body, [1,2,3])
+  ensure
+    File.unlink(__dir__+"/../fixtures/required_tree_test/required_file3.rb") rescue nil
+  end
+
   describe 'source maps' do
     RSpec::Matchers.define :include_source_map do
       match do |actual_response|
