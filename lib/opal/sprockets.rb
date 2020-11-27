35a0fc420c86fa37fae81871c129db62c86cbd93
Elia Schito <elia@schito.me>
2020-11-27 20:10:30 +0100

    Let the main require eagerly load all the pieces
    It's actually important for Sprockets to have the processors loaded
    in order to register them to it.

--- lib/opal/sprockets.rb
+++ lib/opal/sprockets.rb
@@ -3,14 +3,14 @@ require 'opal/sprockets/version'
 
 module Opal
   module Sprockets
-    autoload :AssetsHelper, 'opal/sprockets/assets_helper'
-    autoload :Environment, 'opal/sprockets/environment'
-    autoload :ERB, 'opal/sprockets/erb'
-    autoload :MimeTypes, 'opal/sprockets/mime_types'
-    autoload :Processor, 'opal/sprockets/processor'
-    autoload :Server, 'opal/sprockets/server'
-
+    require 'opal/sprockets/assets_helper'
+    require 'opal/sprockets/mime_types'
     extend AssetsHelper
     extend MimeTypes
+
+    require 'opal/sprockets/environment'
+    require 'opal/sprockets/erb'
+    require 'opal/sprockets/processor'
+    require 'opal/sprockets/server'
   end
 end
